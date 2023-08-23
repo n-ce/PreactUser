1b7ee43ea4a10e950b394bf9b56b4c0609687665
Animesh <69345507+n-ce@users.noreply.github.com>
2023-08-23 21:31:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/PreactUser/'
+  base: '/PreactUser/',
+  build: {
+    target: 'esnext'
+  }
 })
